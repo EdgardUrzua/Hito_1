f85d9a8b37f352646d922f04bb039e029089b563
Edgard <edgardurzuafaundez@gmail.com>
2024-08-07 03:16:57 -0400

    first commit

---
+++ src/componentes/Footer.jsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+    <p>&copy; 2023 - Pizzería Mamma Mia! - Todos los derechos reservados</p>
+  </footer>
+  )
+}
+
+export default Footer;
---
+++ src/componentes/Header.jsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className="text-center mb-4"> 
+      <h1>¡Bienvenidos a Pizzería Mamma Mia!</h1>
+      <p>Las mejores pizzas artesanales de la ciudad.</p>
+    </header>
+  )
+}
+
+export default Header;
